8003690efd9d1ec27a118ef11752a6cbdc50c789
derrekdo <derrekdo123@gmail.com>
2022-11-28 01:27:08 -0800

    Created initializedReservedWords

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        String inputLocation = "." + File.separator + "input-files" + File.separator + "reservedWords.txt";
+        Parser parser = new Parser();
+        parser.initializeReservedWords(inputLocation);
+        parser.setBalancedBST();
+
+        Palindrome.main(null);
+    }
+
 }
--- src/Parser.java
+++ src/Parser.java
@@ -1,22 +1,59 @@
-import java.util.TreeSet;
+import TreePackage.BinarySearchTree.BinarySearchTree;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+
 
 public class Parser {
 
-    TreeSet<String> reservedWords;
+    private ArrayList<String> reservedWords = new ArrayList<>();
+    private BinarySearchTree<String> keyWords = new BinarySearchTree<>();
+    private BinarySearchTree<String> identifiers = new BinarySearchTree<>();
 
-    public Parser(){
+    public void initializeReservedWords(String inputLocation) throws FileNotFoundException {
+        //File object that contains data from the file location
+        File file = new File(inputLocation);
+        //Scanner object to read an input
+        Scanner input = new Scanner(file);
 
-    }
+        //reads each line of the file and adds it to the reservedWords ArrayList
+        //Runtime O(N)
+        while(input.hasNextLine()){
+            reservedWords.add(input.nextLine());
 
-    public void initializeReservedWords(){
+        }
+        input.close();
 
     }
 
     public void setBalancedBST(){
-
+        for(int i = 0; i < reservedWords.size(); i++){
+            keyWords.add(reservedWords.get(i));
+        }
     }
 
     public void getIdentifiers(){
 
     }
+
+//    private ArrayList<String> read() throws FileNotFoundException {
+//        //file location
+//        String inputLocation = "." + File.separator + "input-files" + File.separator + "reservedWords.txt";
+//        //File object that contains data from the file location
+//        File file = new File(inputLocation);
+//        //Scanner object to read an input
+//        Scanner input = new Scanner(file);
+//
+//        //stores each line the file into
+//        //Runtime O(N)
+//        while(input.hasNextLine()){
+//            reservedWords.add(input.nextLine());
+//
+//        }
+//        input.close();
+//        return words;
+//    }
 }
